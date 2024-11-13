50a712797e5b14b33a060dab5b8293c46ec8e191
jaredb96 <goldpineresin124@gmail.com>
2024-11-13 14:28:25 -0500

    Null test.

--- hmda-analytics/src/test/scala/hmda/analytics/HmdaAnalyticsAppSpec.scala
+++ hmda-analytics/src/test/scala/hmda/analytics/HmdaAnalyticsAppSpec.scala
@@ -1,22 +1,15 @@
 package hmda.analytics
 
-import hmda.analytics.HmdaAnalyticsApp.YearlyTransmittalSheetRepositoryWrapper
-import hmda.analytics.query.LarComponent
-import hmda.model.filing.lar.LarGenerators._
-import hmda.model.filing.submission.SubmissionId
-import hmda.utils.EmbeddedPostgres
-import hmda.utils.YearUtils.Period
-import org.scalatest.{AsyncWordSpec, BeforeAndAfter, MustMatchers}
-
+import org.scalatest.{AsyncWordSpec, MustMatchers}
 
 class HmdaAnalyticsAppSpec extends AsyncWordSpec with MustMatchers {
-  val sampleSubmissionId = SubmissionId()
-  val testYearlyWrapper = YearlyTransmittalSheetRepositoryWrapper(sampleSubmissionId.period.year.toString)
-
-  "YearlyWrapper" must {
-    "say hello world" in {
-      val hello = 5
-      assert(hello == 5)
+  "HmdaAnalyticsApp" must
+    {
+      "return hmda logger" in {
+        val app = HmdaAnalyticsApp
+        val logger = HmdaAnalyticsApp.log
+        logger mustEqual null
+      }
     }
-  }
+
 }
