737921f33b740eb61ae62a9c9bfcd97392bca90d
jaredb96 <goldpineresin124@gmail.com>
2024-11-13 14:06:19 -0500

    Sample yearly wrapper.

--- hmda-analytics/src/test/scala/hmda/analytics/HmdaAnalyticsAppSpec.scala
+++ hmda-analytics/src/test/scala/hmda/analytics/HmdaAnalyticsAppSpec.scala
@@ -1,12 +1,15 @@
 package hmda.analytics
 
+import hmda.analytics.HmdaAnalyticsApp.YearlyTransmittalSheetRepositoryWrapper
 import hmda.analytics.query.LarComponent
 import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.submission.SubmissionId
 import hmda.utils.EmbeddedPostgres
+import hmda.utils.YearUtils.Period
 import org.scalatest.{AsyncWordSpec, BeforeAndAfter, MustMatchers}
 
 
 class HmdaAnalyticsAppSpec extends MustMatchers {
-
-
+  val sampleSubmissionId = SubmissionId()
+  val testYearlyWrapper = YearlyTransmittalSheetRepositoryWrapper(sampleSubmissionId.period.year.toString)
 }
