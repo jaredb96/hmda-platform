9a53c0028be35f17169e7072686470f4f3fe54ed
jaredb96 <goldpineresin124@gmail.com>
2024-11-13 13:35:49 -0500

    Hmda analytics spec skeleton.

---
+++ hmda-analytics/src/test/scala/hmda/analytics/HmdaAnalyticsAppSpec.scala
@@ -0,0 +1,12 @@
+package hmda.analytics
+
+import hmda.analytics.query.LarComponent
+import hmda.model.filing.lar.LarGenerators._
+import hmda.utils.EmbeddedPostgres
+import org.scalatest.{AsyncWordSpec, BeforeAndAfter, MustMatchers}
+
+
+class HmdaAnalyticsAppSpec extends MustMatchers {
+
+
+}
