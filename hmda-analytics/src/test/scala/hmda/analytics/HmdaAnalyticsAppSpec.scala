61f1788985c894a69047cdd5b0ec4a6d19e3c27e
jaredb96 <goldpineresin124@gmail.com>
2024-11-13 14:09:18 -0500

    AsyncWordSpec in test class.

--- hmda-analytics/src/test/scala/hmda/analytics/HmdaAnalyticsAppSpec.scala
+++ hmda-analytics/src/test/scala/hmda/analytics/HmdaAnalyticsAppSpec.scala
@@ -9,7 +9,14 @@ import hmda.utils.YearUtils.Period
 import org.scalatest.{AsyncWordSpec, BeforeAndAfter, MustMatchers}
 
 
-class HmdaAnalyticsAppSpec extends MustMatchers {
+class HmdaAnalyticsAppSpec extends AsyncWordSpec with MustMatchers {
   val sampleSubmissionId = SubmissionId()
   val testYearlyWrapper = YearlyTransmittalSheetRepositoryWrapper(sampleSubmissionId.period.year.toString)
+
+  "YearlyWrapper" must {
+    "say hello world" in {
+      val hello = 5
+      assert(hello == 5)
+    }
+  }
 }
